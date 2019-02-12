f04ad618070fbbbb38432cc4d2edce816e13017e
Piotr Stefanowicz <stefan8357@gmail.com>
2019-02-12 19:26:47 +0100

    Adding router fix for github pages

--- src/components/Content.js
+++ src/components/Content.js
@@ -8,10 +8,10 @@ import ChallengeView from "./views/ChallengeView";
 const Content = () => (
   <div>
     <Switch>
-      <Route exact path="/" component={DashboardView}/>
-      <Route path="/dashboard" component={DashboardView}/>
-      <Route path="/recipes" component={RecipesView}/>
-      <Route path="/challenge" component={ChallengeView}/>
+      <Route exact path={`${process.env.PUBLIC_URL}/`} component={DashboardView}/>
+      <Route path={`${process.env.PUBLIC_URL}/dashboard`} component={DashboardView}/>
+      <Route path={`${process.env.PUBLIC_URL}/recipes`} component={RecipesView}/>
+      <Route path={`${process.env.PUBLIC_URL}/challenge`} component={ChallengeView}/>
       <Route render={() => <h1>Not found</h1>}/>
     </Switch>
   </div>
