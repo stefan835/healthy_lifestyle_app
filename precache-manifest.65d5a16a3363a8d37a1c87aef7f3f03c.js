30052eeaa2017de1739a5c36d531ec7505bc35f9
Piotr Stefanowicz <stefan8357@gmail.com>
2019-02-12 12:34:10 +0100

    Updates

---
+++ precache-manifest.65d5a16a3363a8d37a1c87aef7f3f03c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "86541d1463ce9a446290",
+    "url": "/healthy_lifestyle_app/static/js/runtime~main.86541d14.js"
+  },
+  {
+    "revision": "0bf0bd61b381d51ce9e9",
+    "url": "/healthy_lifestyle_app/static/js/main.0bf0bd61.chunk.js"
+  },
+  {
+    "revision": "4021bec0efb9814edce6",
+    "url": "/healthy_lifestyle_app/static/js/1.4021bec0.chunk.js"
+  },
+  {
+    "revision": "0bf0bd61b381d51ce9e9",
+    "url": "/healthy_lifestyle_app/static/css/main.1c73a8b3.chunk.css"
+  },
+  {
+    "revision": "0205419fddb4cedf2eb2414ba1285642",
+    "url": "/healthy_lifestyle_app/index.html"
+  }
+];
